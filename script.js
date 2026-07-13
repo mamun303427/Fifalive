9ac1a07126ab32cd2f160ef249a39692fb35bfc1
mamun303427 <146650966+mamun303427@users.noreply.github.com>
2026-07-13 11:52:38 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ১. কনফিগারেশন
 const wcServers = [
-    { name: "T Sports HD", url: "http://rgkkw.live:80/live/1Aoen7elp5/IgMJ60tmAa/130714.ts" },
+    { name: "T Sports HD", url: "https://bdixserver.tvhunter.top/ant1009/tracks-v1a1/index.m3u8" },
     { name: "T Sports Backup", url: "https://trs1.aynaott.com/tsports/tracks-v1a1/mono.ts.m3u8" },
     { name: "Bein Sports 1", url: "https://1nyaler.streamhostingcdn.top/stream/23/index.m3u8" },
     { name: "Bein Sports 3", url: "https://ua.online24.pm/play/1103/350B326FB34F4B8/video.m3u8" },
